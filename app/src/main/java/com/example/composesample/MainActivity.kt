4ba76c2604a51a0b5d3e4d08e4e3ac7fdd996af0
SB <seungbae2@gmail.com>
2023-02-28 23:11:35 +0900

    Text Sample

--- app/src/main/java/com/example/composesample/MainActivity.kt
+++ app/src/main/java/com/example/composesample/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composesample.ui.theme.ComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +39,66 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+//    Text(text = "Hello $name!")
+
+//    Text(color = Color.Red, text = "Hello $name!")
+
+//    Text(color = Color(0xffff9944), text = "Hello $name!")
+
+//    Text(color = Color.Red, text = "Hello $name!", fontSize = 30.sp)
+
+//    Text(color = Color(0xffff9944), text = "Hello $name!", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+//    Text(
+//        color = Color(0xffff9944),
+//        text = "Hello $name!",
+//        fontSize = 30.sp,
+//        fontWeight = FontWeight.Bold,
+//        fontFamily = FontFamily.Cursive
+//    )
+
+//    Text(
+//        color = Color(0xffff9944),
+//        text = "Hello $name!",
+//        fontSize = 30.sp,
+//        fontWeight = FontWeight.Bold,
+//        fontFamily = FontFamily.Cursive,
+//        letterSpacing = 2.sp
+//    )
+
+//    Text(
+//        color = Color(0xffff9944),
+//        text = "Hello $name!\nHello $name!\nHello $name!",
+//        fontSize = 30.sp,
+//        fontWeight = FontWeight.Bold,
+//        fontFamily = FontFamily.Cursive,
+//        letterSpacing = 2.sp,
+//        maxLines = 2
+//    )
+
+//    Text(
+//        color = Color(0xffff9944),
+//        text = "Hello $name!\nHello $name!\nHello $name!",
+//        fontSize = 30.sp,
+//        fontWeight = FontWeight.Bold,
+//        fontFamily = FontFamily.Cursive,
+//        letterSpacing = 2.sp,
+//        maxLines = 2,
+//        textDecoration = TextDecoration.Underline
+//    )
+
+    Text(
+        modifier = Modifier.size(300.dp),
+        color = Color(0xffff9944),
+        text = "Hello $name!\nHello $name!\nHello $name!",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        letterSpacing = 2.sp,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Preview(showBackground = true)
