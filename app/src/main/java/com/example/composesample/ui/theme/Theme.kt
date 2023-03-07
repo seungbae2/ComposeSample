5da08dea8080dc059dad1a8c22b5290bcdba4ea7
SB <seungbae2@gmail.com>
2023-03-07 20:54:38 +0900

    Column Sample

--- app/src/main/java/com/example/composesample/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composesample/ui/theme/Theme.kt
@@ -125,3 +125,19 @@ fun RowTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable ()
         content = content
     )
 }
+
+@Composable
+fun ColumnTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
