b60aaa0bf6fb5e46eee3d4442694bae18c281424
SB <seungbae2@gmail.com>
2023-05-01 23:21:57 +0900

    Catalog example

--- app/src/main/java/com/example/composesample/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composesample/ui/theme/Theme.kt
@@ -287,3 +287,19 @@ fun ScaffoldTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
         content = content
     )
 }
+
+@Composable
+fun CatalogTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
